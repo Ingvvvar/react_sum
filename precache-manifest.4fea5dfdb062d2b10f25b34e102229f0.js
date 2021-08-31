1609a239bec600bd772f15ed73efeff65ca3f35c
Igor <2492345@gmail.com>
2021-08-31 18:22:34 +0300

    Updates

---
+++ precache-manifest.4fea5dfdb062d2b10f25b34e102229f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9855bb915d4518df664bdb91edbf272",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69a83d14d376ad490df3",
+    "url": "./static/css/main.84f2e856.chunk.css"
+  },
+  {
+    "revision": "356a3025958f3fc0ce11",
+    "url": "./static/js/2.2b8886b4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2b8886b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a83d14d376ad490df3",
+    "url": "./static/js/main.2b780a19.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
